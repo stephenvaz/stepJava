73f0a5651a0d850068ba957692775fcc1e110c04
Stephen Vaz <54773712+stephenvaz@users.noreply.github.com>
2022-07-19 16:00:47 +0530

    l10 q2 done

---
+++ L10/q2/myPackage.java
@@ -0,0 +1,19 @@
+package q2;
+
+public class myPackage {
+    public double calculateGST(int type, double amount) {
+        switch(type) {
+            case 1:
+                return amount * 0.05;
+            case 2:
+                return amount * 0.1;
+            case 3:
+                return amount * 0.12;
+            case 4:
+                return amount * 0.15;
+            default:
+                return 0;
+        }
+    }
+    
+}
---
+++ L10/q2c.java
@@ -0,0 +1,31 @@
+import q2.myPackage;
+import java.util.Scanner;
+
+class q2c {
+    static void eR() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void main(String[] args) {
+        myPackage gst = new myPackage();
+        Scanner sC = new Scanner(System.in);
+        while (true) {
+            System.out.printf("1.CGST\n2.SGST\n3.UTGST\n4.IGST\n5.Exit\n");
+            int n = sC.nextInt();
+            if (n == 5) {
+                return;
+            } else {
+                eR();
+                System.out.printf("Enter the amount: ");
+                double amt = sC.nextDouble();
+                System.out.printf("Tax: %.2f\n", gst.calculateGST(n, amt));
+
+                sC.nextLine();
+                sC.nextLine();
+                eR();
+            }
+
+        }
+    }
+}
